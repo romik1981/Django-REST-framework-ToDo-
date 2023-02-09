5654c04b455b2c5c34f4be98b7d8529128754825
roman <belyakov1981@mail.ru>
2023-02-09 17:56:54 +0300

    First commit

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
-from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
 from uuid import uuid4
 
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
 # Create your models here.
+
 
 class User(AbstractBaseUser):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
