af27c05ee0c276ea372f89659a75be24f68331c6
roman <belyakov1981@mail.ru>
2023-02-10 15:08:24 +0300

    lesson_2_update_models.

--- users/models.py
+++ users/models.py
@@ -1,17 +1,22 @@
-from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
 from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 
-class User(AbstractBaseUser):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    first_name = models.CharField(max_length=64, blank=True)
-    last_name = models.CharField(max_length=64, blank=True)
-    birthday_year = models.PositiveIntegerField(null=True, blank=True)
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name="UUID")
+    username = models.CharField(unique=True, max_length=64, null=True, verbose_name="имя пользователя")
+    first_name = models.CharField(max_length=64, blank=True, verbose_name="имя")
+    last_name = models.CharField(max_length=64, blank=True, verbose_name="фамилия")
+    birthday_year = models.PositiveIntegerField(null=True, blank=True, verbose_name="год рождения")
     email = models.CharField(
         max_length=256,
         unique=True,
+        null=False,
+        verbose_name="электронная почта",
         error_messages={
             "unique": "A user with that email address already exists.",
         },
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
-#from django.shortcuts import render
+# from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+
 from .models import User
 from .serializers import UserModelSerializer
 
 # Create your views here.
+
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
