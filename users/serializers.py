2d53b21a496db192d35c9a5791830b3e1a94578e
romik1981 <87126846+romik1981@users.noreply.github.com>
2023-02-11 18:14:03 +0300

    Delete serializers.py

--- users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-
-from .models import User
-
-
-class UserModelSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["first_name", "last_name", "birthday_year", "email"]
