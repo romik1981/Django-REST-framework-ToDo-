a2e6367d2238b8a65e49fc663e6cc038bba1d756
roman <belyakov1981@mail.ru>
2023-03-09 16:34:04 +0300

    create ToDoapp

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from ToDoapp.views import ProjectViewSet, ToDoViewSet
 from users.views import UserModelViewSet
 
 router = DefaultRouter()
 router.register("users", UserModelViewSet)
+router.register("projects", ProjectViewSet)
+router.register("todos", ToDoViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
